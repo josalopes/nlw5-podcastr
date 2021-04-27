3f4c6384f3517a9c697abf1f75976723751f23e3
Francisco Josafá <josafa.lopes@gmail.com>
2021-04-27 17:41:01 -0300

    Implementado primeira versao do build do projeto

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,33 @@
 import Head from 'next/head'
-import { Header } from './components/Header'
+import { useEffect } from 'react';
 
 export default function Home() {
+
   return (
       <h1>Index</h1>
 
   )
+}
+
+// export async function getServerSideProps() {
+//   const response = await fetch('http://localhost:3333/episodes');
+//   const data = await response.json();
+
+//   return {
+//     props: {
+//       episodes: data,
+//     }
+//   }
+// }
+
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes');
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data,
+      revalidate: 60 * 60 * 8,
+    }
+  }
 }
