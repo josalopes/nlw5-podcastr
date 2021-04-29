b2795e2a986efcb12a5b56f01cc9963fa4773750
Francisco Josafá <josafa.lopes@gmail.com>
2021-04-28 23:49:33 -0300

    Implementada a pagina que exibe os detalhes de um podcast e e permite sua execuçao

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import { GetStaticProps } from 'next';
 import { format, parseISO } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
 import Image from 'next/image';
+import Link from 'next/link';
 
 import Head from 'next/head'
 import { api } from '../../src/services/api';
@@ -44,7 +45,9 @@ export default function Home({ latestEpisodes, allEpisodes }: Homeprops) {
                   />
 
                   <div className={styles.episodeDetails}>
-                    <a href="">{episode.title}</a>
+                    <Link href={`/episodes/${episode.id}`}>
+                      <a>{episode.title}</a>
+                    </Link>
                     <p>{episode.members}</p>
                     <span>{episode.publishedAt}</span>
                     <span>{episode.durationAsString}</span>
@@ -60,23 +63,54 @@ export default function Home({ latestEpisodes, allEpisodes }: Homeprops) {
       </section>
 
       <section className={styles.allEpisodes}>
+        <h2>Todos os episódios</h2>
 
+        <table cellSpacing={0}>
+          <thead>
+            <th></th>
+            <th>Podcast</th>
+            <th>Integrantes</th>
+            <th>Data</th>
+            <th>Duração</th>
+            <th></th>
+          </thead>
+          <tbody>
+            {allEpisodes.map(episode => {
+              return (
+                <tr key={episode.id}>
+                  <td style={{ width: 72}}>
+                    <Image 
+                      width={120} 
+                      height={120} 
+                      objectFit="cover"
+                      src={episode.thumbnail} 
+                      alt={episode.title} 
+                    />
+                  </td>
+                  <td>
+                    <Link href={`/episodes/${episode.id}`}>
+                      <a>{episode.title}</a>
+                    </Link>
+                  </td>
+                    <td>{episode.members}</td>
+                    <td style={{ width: 100}}>{episode.publishedAt}</td>
+                    <td>{episode.durationAsString}</td>
+                    <td>
+                      <button type="button">
+                        <img src="/play-green.svg" alt="Tocar episódio"/>
+                      </button>
+                    </td>
+                </tr>
+
+              )
+            })}
+          </tbody>
+        </table>
       </section>
     </div>
 
   )
 }
-
-// export async function getServerSideProps() {
-//   const response = await fetch('http://localhost:3333/episodes?_limit=12&_sort=published_at&_order=desc');
-//   const data = await response.json();
-
-//   return {
-//     props: {
-//       episodes: data,
-//     }
-//   }
-// }
 
 export const getStaticProps: GetStaticProps = async () => {
   const response = await api.get('episodes', {
